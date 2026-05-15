4ecf61d345a45245a04cedfcbb814c1eed30b27f
li0000001 <ljt158369@gmail.com>
2026-05-15 11:37:50 +0800

    Allow user scaling and increase maximum scale

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  maximumScale: 5,
+  userScalable: true,
   themeColor: "#f0ebe3",
 };
 
